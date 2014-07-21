871b72ad1eb6a800f1c7c463117a9fe87a68cfd8
Amin Meyghani <amin.meyghani@quovo.com>
2014-07-21 01:52:06 -0400

    remove old puppet

--- xpuppet/files/install-grunt.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-if [ ! -f /usr/local/bin/grunt ]
-	then
-	echo "installing grunt ..." >> /home/vagrant/puppetlog.txt
-	/usr/local/bin/npm install -g grunt-cli
-else
-	echo "grunt was already installed"  >> /home/vagrant/puppetlog.txt
-	exit 0
-fi
