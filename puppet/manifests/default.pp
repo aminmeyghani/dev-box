4ee567e495db1a9f2d97a0ec01f2999bcf329eaf
Amin Meyghani <amin.meyghani@quovo.com>
2014-07-21 01:25:01 -0400

    add ruby and rails manifests

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -3,7 +3,7 @@ $as_vagrant   = 'sudo -u vagrant -H bash -l -c'
 $home         = '/home/vagrant'
 
 Exec {
-  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin', '/home/vagrant']
 }
 
 # --- Preinstall Stage ---------------------------------------------------------
@@ -12,66 +12,76 @@ stage { 'preinstall':
   before => Stage['main']
 }
 
-# create a the shell scripts directory.      
-file { "/home/vagrant/scripts":
-    ensure => "directory",
+# update and the pre-install folders.
+class update_and_preinstall {
+
+  # exec { 'Update Aptitude sources':
+  #   command => 'apt-get update'
+  # }
+  exec { 'apt-get update': }
+
+  # create a the shell scripts directory.      
+  file { "/home/vagrant/scripts":
+      ensure => "directory"
+  }
+  # set up shared folder
+  file { "/home/vagrant/shared_folder":
+      ensure => "directory",
+  }
+
+  # set up the log file
+  file { "/home/vagrant/puppetlog.txt":
+      ensure => "present",
+      group => "vagrant",
+      mode => 0777
+  }
+  # add the rails install script
+  file { "/home/vagrant/scripts/install-rails.sh":
+    source => "/vagrant/puppet/files/install-rails.sh",
+    owner => "vagrant", group => "vagrant", mode => 0755;
+  }
+  # install node.
+  file {
+    "/home/vagrant/scripts/install-node.sh":
+    source => "/vagrant/puppet/files/install-node.sh",
+    owner => "vagrant", group => "vagrant", mode => 0755;
+  }
+  # copy over the uninstall node scropt
+  file {
+    "/home/vagrant/scripts/uninstall-node.sh":
+    source => "/vagrant/puppet/files/uninstall-node.sh",
+    owner => "vagrant", group => "vagrant", mode => 0755;
+  }
+  # copy over the install grunt shell script.
+  file {
+    "/home/vagrant/scripts/install-grunt.sh":
+    source => "/vagrant/puppet/files/install-grunt.sh",
+    owner => "vagrant", group => "vagrant", mode => 0755;
+  }
+  # set up the www symbolic link to /var/www
+  file { "/home/vagrant/shared_folder/www":
+      ensure => "directory",
+  }
+
+  # set the symlink only if the apache script is ran.
+  file { '/var/www':
+     ensure => 'link',
+     target => '/home/vagrant/shared_folder/www',
+     force => true,
+  }
+
+  # copy over the install php + apache.
+  file { "/home/vagrant/scripts/install-lamp.sh":
+    source => "/vagrant/puppet/files/install-lamp.sh",
+    owner => "vagrant", group => "vagrant", mode => 0755;
+  }
+}
+class { 'update_and_preinstall':
+  stage => preinstall
 }
 
-# set up shared folder
-file { "/home/vagrant/shared_folder":
-    ensure => "directory",
-}
-
-# set up the www symbolic link to /var/www
-file { "/home/vagrant/shared_folder/www":
-    ensure => "directory",
-}
-
-
-
-# set the symlink only if the apache script is ran.
-file { '/var/www':
-   ensure => 'link',
-   target => '/home/vagrant/shared_folder/www',
-   force => true,
-}
-
-
-
-# set up the log file
-file { "/home/vagrant/puppetlog.txt":
-    ensure => "present",
-    group => "vagrant",
-    mode => 0777
-}
-
+# --- Installing node. ---------------------------------------------------------
 # copy over the install node script.
-file {
-  "/home/vagrant/scripts/install-node.sh":
-  source => "/vagrant/puppet/files/install-node.sh",
-  owner => "vagrant", group => "vagrant", mode => 0755;
-}
-
-# copy over the uninstall node scropt
-file {
-  "/home/vagrant/scripts/uninstall-node.sh":
-  source => "/vagrant/puppet/files/uninstall-node.sh",
-  owner => "vagrant", group => "vagrant", mode => 0755;
-}
-
-# copy over the install grunt shell script.
-file {
-  "/home/vagrant/scripts/install-grunt.sh":
-  source => "/vagrant/puppet/files/install-grunt.sh",
-  owner => "vagrant", group => "vagrant", mode => 0755;
-}
-
-# copy over the install php + apache.
-file {
-  "/home/vagrant/scripts/install-lamp.sh":
-  source => "/vagrant/puppet/files/install-lamp.sh",
-  owner => "vagrant", group => "vagrant", mode => 0755;
-}
 
 class installnode {
   exec { '/home/vagrant/scripts/install-node.sh': 
@@ -79,6 +89,8 @@ class installnode {
   }
 }
 class { 'installnode': }
+
+# # # --- Installing grunt. ---------------------------------------------------------
 
 class installgrunt {
   require installnode
@@ -88,23 +100,32 @@ class installgrunt {
 }
 class { 'installgrunt': }
 
-# run update
-class apt_get_update {
-  exec { 'apt-get -y update':
-    unless => "test -e ${home}/.rvm"
-  }
-}
-class { 'apt_get_update':
-  stage => preinstall
+# # # --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
 }
 
-# --- SQLite -------------------------------------------------------------------
+package { 'build-essential':
+  ensure => installed
+}
 
-# package { ['sqlite3', 'libsqlite3-dev']:
-#   ensure => installed;
-# }
+package { 'git-core':
+  ensure => installed
+}
 
-# --- MySQL --------------------------------------------------------------------
+# vim is not obviously required.
+package { 'vim':
+  ensure => installed
+}
+
+# # # --- SQLite -------------------------------------------------------------------
+
+package { ['sqlite3', 'libsqlite3-dev']:
+  ensure => installed;
+}
+
+# # --- MySQL --------------------------------------------------------------------
 
 class install_mysql {
   class { 'mysql': }
@@ -135,11 +156,11 @@ class install_mysql {
 }
 class { 'install_mysql': }
 
-# --- PHP + APACHE ---------------------------------------------------------------  
-# exec { '/home/vagrant/scripts/install-lamp.sh': require => File['/home/vagrant/scripts/install-lamp.sh'] }
+# # --- PHP + APACHE ---------------------------------------------------------------  
+
 class installlamp {
-  require apt_get_update
-  require install_mysql
+  # require apt_get_update
+  # require install_mysql
   exec { '/home/vagrant/scripts/install-lamp.sh': 
     require => File['/home/vagrant/scripts/install-lamp.sh'] 
   }
@@ -147,100 +168,95 @@ class installlamp {
 class { 'installlamp': }
 
 
-# --- PostgreSQL ---------------------------------------------------------------
+# # --- PostgreSQL ---------------------------------------------------------------
 
-# class install_postgres {
-#   class { 'postgresql': }
+# # class install_postgres {
+# #   class { 'postgresql': }
 
-#   class { 'postgresql::server': }
+# #   class { 'postgresql::server': }
 
-#   pg_database { $ar_databases:
-#     ensure   => present,
-#     encoding => 'UTF8',
-#     require  => Class['postgresql::server']
-#   }
+# #   pg_database { $ar_databases:
+# #     ensure   => present,
+# #     encoding => 'UTF8',
+# #     require  => Class['postgresql::server']
+# #   }
 
-#   pg_user { 'rails':
-#     ensure  => present,
-#     require => Class['postgresql::server']
-#   }
+# #   pg_user { 'rails':
+# #     ensure  => present,
+# #     require => Class['postgresql::server']
+# #   }
 
-#   pg_user { 'vagrant':
-#     ensure    => present,
-#     superuser => true,
-#     require   => Class['postgresql::server']
-#   }
+# #   pg_user { 'vagrant':
+# #     ensure    => present,
+# #     superuser => true,
+# #     require   => Class['postgresql::server']
+# #   }
 
-#   package { 'libpq-dev':
-#     ensure => installed
-#   }
-# }
-# class { 'install_postgres': }
+# #   package { 'libpq-dev':
+# #     ensure => installed
+# #   }
+# # }
+# # class { 'install_postgres': }
 
-# --- Memcached ----------------------------------------------------------------
+# # --- Memcached ----------------------------------------------------------------
 
-# class { 'memcached': }
+# # class { 'memcached': }
 
-# --- Packages -----------------------------------------------------------------
+# # Nokogiri dependencies.
+# # package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+# #   ensure => installed
+# # }
 
-package { 'curl':
-  ensure => installed
+# # --- Ruby and Rails using rbenv ---------------------------------------------------------------------
+
+# Installs rbenv
+exec { 'install_rbenv':
+  command => "${as_vagrant} 'curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash'",
+  creates => "${home}/.rbenv",
+  require => Package['curl']
 }
 
-package { 'build-essential':
-  ensure => installed
+# Installing Ruby.
+exec { 'install_ruby':
+  command => "${as_vagrant} '${home}/.rbenv/bin/rbenv install 2.1.0'",
+  creates => "${home}/.rbenv/versions/2.1.0",
+  # in case the build takes a while.
+  timeout => 0,
+  require => Exec['install_rbenv']
 }
 
-package { 'git-core':
-  ensure => installed
+# Set Ruby 2.1.0 as global.
+exec { 'setruby_global':
+  command => "${as_vagrant} '${home}/.rbenv/bin/rbenv global 2.1.0'",
+  require => Exec['install_ruby']
 }
 
-package { 'vim':
-  ensure => installed
+# Installing rails
+exec { "${as_vagrant} 'gem install rails --no-rdoc --no-ri'":
+  creates => "${home}/.rbenv/shims/rails",
+  timeout => 0,
+  require => Exec['setruby_global']
 }
 
-# Nokogiri dependencies.
-# package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
-#   ensure => installed
+# Sets the path for rbenv.
+file {
+  "/etc/profile.d/set_rbenv_path.sh":
+  source => "/vagrant/puppet/files/set_rbenv_path.sh",
+  owner => "vagrant", 
+  group => "vagrant", 
+  ensure => "present",
+  mode => 0755;
+}
+
+
+# ----------using notificaton test----------------------------
+# using notification
+# notify { 'some-command':
+#   message => 'some-command is going to be executed now'
 # }
 
-# # ExecJS runtime.
-# package { 'nodejs':
-#   ensure => installed
+# exec { 'some-command':
+#   command => 'echo hello',
 # }
 
-# --- Ruby ---------------------------------------------------------------------
-
-# exec { 'install_rvm':
-#   command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
-#   creates => "${home}/.rvm",
-#   require => Package['curl']
-# }
-
-# exec { 'install_ruby':
-#   # We run the rvm executable directly because the shell function assumes an
-#   # interactive environment, in particular to display messages or ask questions.
-#   # The rvm executable is more suitable for automated installs.
-#   #
-#   # Thanks to @mpapis for this tip.
-#   command => "${as_vagrant} '${home}/.rvm/bin/rvm install 2.0.0 --latest-binary --autolibs=enabled && rvm --fuzzy alias create default 2.0.0'",
-#   creates => "${home}/.rvm/bin/ruby",
-#   require => Exec['install_rvm']
-# }
-
-# exec { "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'":
-#   creates => "${home}/.rvm/bin/bundle",
-#   require => Exec['install_ruby']
-# }
-
-# exec { "${as_vagrant} 'gem install rails --version=3.2.14 --no-rdoc --no-ri'":
-#   creates => "${home}/.rvm/bin/rails",
-#   require => Exec['install_ruby']
-# }
-
-# file {
-#   "/home/vagrant/.bash_profile":
-#   source => "/vagrant/puppet/files/bash_profile",
-#   owner => "vagrant", group => "vagrant", mode => 0664;
-# }
-
+# Notify['some-command'] -> Exec['some-command']
